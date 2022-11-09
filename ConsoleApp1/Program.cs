c068940d0250809f3f8af342f40c08b9dd83ee17
ispan <ispan@DESKTOP-PAKSETB>
2022-11-09 10:48:04 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> items = new List<int> { 5, 11, 4, 9, 66, 48, 35 };
+            Func<int, bool> isEven = n => n % 2 == 0;
+
+            List<int> EvenItems = GetEvenitems(items, isEven);
+
+        }
+        static List<int> GetEvenitems(List<int> source, Func<int, bool> func)
+        {
+            var result = new List<int>();
+
+            foreach (int item in source)
+            {
+                if (func(item) == true)
+                {
+                    result.Add(item);
+                }
+                return result;
+            }
+
+        }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> items = new List<int>{5,11, 4, 9, 66,48,35};
+            Func<int, bool> isEven = n => n % 2 == 0;
+
+            List<int> EvenItems = GetEvenItems(items,isEven);
+           
+        }
+        static List<int> GetEvenItems(List<int> source,Func<int, bool> func){
+            var result=new List<int>();
+
+            foreach (int item in source)
+            {
+                if(func(item)==true)
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
+    }
+
+}
